291e58b8de4ce272ca8dbb69885fd75717fe791f
Keanu Valencia <104657054+keanudv@users.noreply.github.com>
2024-09-11 13:34:21 -1000

    Update Python_code.py

--- Python_code.py
+++ Python_code.py
@@ -11,8 +11,22 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Load the data set into Python
-file_location = "location"
+file_location = "C:\\Users\\keanu\\OneDrive\\Desktop\\School\\BUS 310\\BUS 310 Project 1\\"
 df = pd.read_csv(file_location, encoding="UTF-8")
 
 # View the data
 df.head(10)
+
+# Creates a Histogram to show the spread of the data
+sns.histplot(data=df, bins=30, kde=False)
+plt.title("Title")
+plt.xlabel("Bins of the Histogram")
+plt.ylabel("Y Axis")
+plt.show()
+
+# Creates a Scatter Plot to show any outliers
+sns.scatterplot(data=df, x="x", y="y")
+plt.title("Title")
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+plt.show()
